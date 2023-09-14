aa41be6b0f9c1413aa85c1be03ed856ecd1d43b7
Enrique Fuenzalida <114259706+enriquefuenzalida@users.noreply.github.com>
2023-09-14 04:00:15 -0300

    habitaciones reserva stilo

--- src/app/habitsreserva/habitsreserva.page.spec.ts
+++ src/app/habitsreserva/habitsreserva.page.spec.ts
@@ -5,11 +5,11 @@ describe('HabitsreservaPage', () => {
   let component: HabitsreservaPage;
   let fixture: ComponentFixture<HabitsreservaPage>;
 
-  beforeEach(async(() => {
+  beforeEach(async() => {
     fixture = TestBed.createComponent(HabitsreservaPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
